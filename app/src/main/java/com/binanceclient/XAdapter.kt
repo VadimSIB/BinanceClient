422a695f06b5e280c4780a7d72196357662a28c1
genry <zaskulnikov@gmail.com>
2021-06-17 21:10:38 +0700

    17.062021. Redesigned the adapter class to ViewBinding

--- app/src/main/java/com/binanceclient/XAdapter.kt
+++ app/src/main/java/com/binanceclient/XAdapter.kt
@@ -2,43 +2,36 @@ package com.binanceclient
 
 import android.graphics.Color
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.binanceclient.R.*
+import com.binanceclient.databinding.FragmentXCellBinding
 import java.math.BigDecimal
 
 
-
-class XAdapter(_data: List<MutableMap.MutableEntry<BigDecimal, BigDecimal>>, val _color: String): RecyclerView.Adapter<XAdapter.ItemViewHolder>() {
+class XAdapter(_data: List<MutableMap.MutableEntry<BigDecimal, BigDecimal>>, _color: String): RecyclerView.Adapter<XAdapter.ItemViewHolder>() {
     private var data: List<MutableMap.MutableEntry<BigDecimal, BigDecimal>>
-
-    init {this.data = _data}
+    private var color: String
+    init {this.data = _data; this.color = _color}
     fun setData(_data: List<MutableMap.MutableEntry<BigDecimal, BigDecimal>>){this.data = _data}
 
-    class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val amountView: TextView
-        val priceView: TextView
-        val totalView: TextView
-        init {
-            amountView = itemView.findViewById(id.amount_text)
-            priceView = itemView.findViewById(id.price_text)
-            totalView = itemView.findViewById(id.total_text)
+    inner class ItemViewHolder(private val itemBinding: FragmentXCellBinding) : RecyclerView.ViewHolder(itemBinding.root) {
+
+        fun bind(order: MutableMap.MutableEntry<BigDecimal, BigDecimal>){
+            itemBinding.amountText.text = String.format("%8.4f", order.value)
+            itemBinding.priceText.text =  String.format("%11.6f", order.key)
+            itemBinding.totalText.text = String.format("%11.6f", order.key*order.value)
+            itemBinding.priceText.setTextColor(Color.parseColor(color))
         }
     }
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ItemViewHolder {
-        val view = LayoutInflater.from(viewGroup.context)
-            .inflate(layout.fragment_x_cell, viewGroup, false)
-        return ItemViewHolder(view)
+        val itemBinding = FragmentXCellBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false)
+        return ItemViewHolder(itemBinding)
     }
 
     override fun onBindViewHolder(itemViewHolder: ItemViewHolder, position: Int) {
-        itemViewHolder.amountView.text = String.format("%8.4f", data[position].value)
-        itemViewHolder.priceView.text = String.format("%11.6f", data[position].key)
-        itemViewHolder.totalView.text = String.format("%11.6f", data[position].key*data[position].value)
-        itemViewHolder.priceView.setTextColor(Color.parseColor(_color))
+        val order: MutableMap.MutableEntry<BigDecimal, BigDecimal> = data[position]
+        itemViewHolder.bind(order)
     }
 
     override fun getItemCount() = data.size
